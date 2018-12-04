8aace614c3ca990b19595a42b8613669595481ea
XCode <xcode@sala6pc24.cic>
2018-12-04 17:37:45 +0100

    Initial Commit

---
+++ pracFrameworks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pracFrameworks
+//
+//  Created by XCode on 4/12/18.
+//  Copyright © 2018 XCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
